cd1011c490b8f5e67feb5643e3e211ef9e504ef1
NgallaJuke <ngalladiouck@gmail.com>
2020-01-13 23:39:07 +0000

    Update the Weekly weather Ui

--- JS/forcast.js
+++ JS/forcast.js
@@ -8,49 +8,27 @@ const getCity = async city => {
 
   const response = await fetch(cityURI + query);
   const data = await response.json();
-  console.log("city detail", data[0]);
   return data[0];
 };
 
-const geolocationURI = async () => {
-  const geoURi =
-    "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
-
+const setGeolocation = () => {
   // enable the geolocalisation by the navigator
   if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      async position => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        let query = `?apikey=${key}&q=${latitude},${longitude}`;
-        const response = await fetch(geoURi + query);
-        const data = await response.json();
-        console.log(data);
-        return data;
-      },
-      error => {
-        console.log(error.message);
-        // The user as refuse to activated the geolocation in his browser
-        // So in here we gonna show the shearch bar INPUt
-        // so that the user can type his onw location in Senegal
-        /* Or we can just leave the search bar in the page and display a message saying to search for
-        a location in the search bar */
-      }
-    );
-  } else {
-    query = `?apikey=${key}&q=13.950,-16.167`;
-    const response = await fetch(geoURi + query);
-    const data = await response.json();
-    console.log(data);
-    return data;
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
   }
+  //  else {
+  //   query = `?apikey=${key}&q=13.950,-16.167`;
+  //   const response = await fetch(geoURi + query);
+  //   const data = await response.json();
+  //   console.log(data);
+  //   return data;
+  // }
 };
 
 const getCurrentWeahter = async id => {
   const query = `${id}?apikey=${key}`;
   const response = await fetch(weatherURI + query);
   const data = await response.json();
-  console.log(data[0]);
   return data[0];
 };
 
@@ -60,7 +38,6 @@ const getWeekWeahter = async id => {
   const query = `${id}?apikey=${key}`;
   const response = await fetch(weekWeatherURI + query);
   const data = await response.json();
-  console.log(data.DailyForecasts);
   return data.DailyForecasts;
 };
 
@@ -70,6 +47,20 @@ const getHourWeahter = async id => {
   const query = `${id}?apikey=${key}`;
   const response = await fetch(hourWeatherURI + query);
   const data = await response.json();
-  console.log(data);
   return data;
+};
+
+const setPosition = async position => {
+  const geoURi =
+    "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search";
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const query = `?apikey=${key}&q=${latitude},${longitude}`;
+  const response = await fetch(geoURi + query);
+  const data = await response.json();
+  updateCityName(data);
+};
+
+const showError = err => {
+  console.log(err);
 };
---
+++ JS/ui.js
@@ -0,0 +1,132 @@
+const region_country = document.querySelector("#rg-cn");
+const current_day_hour = document.querySelector("#curt-day-hour");
+const weather_condition = document.querySelector("#weather-condition");
+const weather_icon = document.querySelector(".weather img");
+const weather_temp = document.querySelector(".weather");
+const precip = document.querySelector(".precip span");
+const daily_weather = document.querySelector(".items-weeks");
+//set tthe options to get the current day in a better format
+const options = {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false
+};
+
+const currentDayAndHour = new Date().toLocaleTimeString("en-us", options);
+
+const day = currentDayAndHour.indexOf(",");
+
+const hour = currentDayAndHour.lastIndexOf(",");
+
+const currentDay = currentDayAndHour.substring(0, day);
+const currentHour = currentDayAndHour.substring(hour + 2, hour + 7);
+
+const updateCurrentWeather = async () => {
+  const cityDetails = await getCity("Dakar");
+  console.log("cityDetails", cityDetails);
+  region_country.innerHTML = ` ${cityDetails.AdministrativeArea.EnglishName}, ${cityDetails.Country.EnglishName}`;
+
+  const weatherDetails = await getCurrentWeahter(cityDetails.Key);
+  console.log("weatherDetails", weatherDetails);
+
+  //set the precipitation if there is any
+  if (weatherDetails.HasPrecipitation === false)
+    weatherDetails.HasPrecipitation = 0;
+  precip.innerHTML = `${weatherDetails.HasPrecipitation}%`;
+  current_day_hour.innerHTML = `${currentDay} , ${currentHour}`;
+  weather_condition.innerHTML = `${weatherDetails.WeatherText}`;
+
+  weather_temp.innerHTML = `<img src="./Assets/icons/${weatherDetails.WeatherIcon}.svg" alt="w-icon" />${weatherDetails.Temperature.Metric.Value}
+  <span id="deg">&deg;</span>
+  <span id="celcius">${weatherDetails.Temperature.Metric.Unit}
+  </span>
+  `;
+
+  if (weatherDetails.PrecipitationType)
+    precip.innerHTML = `${weatherDetails.PrecipitationType}`;
+};
+
+updateCurrentWeather();
+
+const updateWeeklyWeather = async () => {
+  const weatherDetails = await getCity("Dakar");
+  const key = weatherDetails.Key;
+  const weekWeatherDetails = await getWeekWeahter(key);
+  console.log("weekWeatherDetails", weekWeatherDetails);
+
+  weekWeatherDetails.forEach(dailyweather => {
+    //Turn the Precipitaion to 0 if there is none
+    if (dailyweather.Day.HasPrecipitation === false)
+      dailyweather.Day.HasPrecipitation = 0;
+    if (dailyweather.Night.HasPrecipitation === false)
+      dailyweather.Night.HasPrecipitation = 0;
+
+    //setting the day on the week for the list of weekly weather
+    const dayOfTheWeek = new Date(dailyweather.Date)
+      .toDateString()
+      .substring(0, 3);
+
+    daily_weather.innerHTML += `<div class="item-week">
+   <div class="day-temps">
+     ${dayOfTheWeek}
+    
+     <span id="temp-Max">
+     ${((dailyweather.Temperature.Maximum.Value - 32) * (5 / 9)).toFixed(
+       1
+     )}&deg;
+     </span>
+     <span id="temp-Min">
+       ${((dailyweather.Temperature.Minimum.Value - 32) * (5 / 9)).toFixed(
+         1
+       )}&deg;
+     </span>
+   </div>
+   <div class="rowsWheather">
+     <div class="icons-Day-Night">
+       <div class="day-In-Week">
+         Day
+       </div>
+       <div>
+         <img src="./Assets/icons/${
+           dailyweather.Day.Icon
+         }.svg" alt="icon-Day" id="icon-Day" />
+       </div>
+       <div class="day-In-Week-Cond">${dailyweather.Day.IconPhrase}</div>
+       <div class="wind-precip">
+         <img
+           class="precipMin"
+           src="./Assets/percipitation.svg"
+           alt="precip"
+         />${dailyweather.Day.HasPrecipitation}%
+       </div>
+     </div>
+     <div class="icons-Day-Night">
+       <div class="day-In-Week">
+         Night
+       </div>
+       <div>
+         <img src="./Assets/icons/${
+           dailyweather.Night.Icon
+         }.svg" alt="icon-Day" id="icon-night" />
+       </div>
+       <div class="day-In-Week-Cond">${dailyweather.Night.IconPhrase}</div>
+       <div class="wind-precip">
+         <img
+           class="precipMin"
+           src="./Assets/percipitation.svg"
+           alt="precip"
+         />${dailyweather.Night.HasPrecipitation}%
+       </div>
+     </div>
+   </div>
+ </div>`;
+  });
+  //
+};
+
+updateWeeklyWeather();
